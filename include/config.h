83cb73db43c884f3885129b425cbcefa3000c604
Dan Luca <danluca22@gmail.com>
2024-02-27 20:14:19 -0600

    updated config for boards

--- include/config.h
+++ include/config.h
@@ -63,7 +63,7 @@
 #if BOARD_ID == 2
 
 // static IP - alternatively, the router can be configured to reserve IPs based on MAC
-#define IP_ADDR 192,168,0,11    //Board 2
+#define IP_ADDR 192,168,0,14    //Board 2
 // measured 3V3 pin voltage (in V and mV)
 #define V3_3    3.242
 #define MV3_3    3242
@@ -73,7 +73,18 @@
 // measured RP2040 internal temp sensor parameters - see RP2040 datasheet page 565
 #define CHIP_RP2040_TEMP_SENSOR_VOLTAGE_27      604.499363f     //in mV
 #define CHIP_RP2040_TEMP_SENSOR_VOLTAGE_SLOPE   1.807087f       //in mV/degree (value is negative, but that is accounted in the formula)
-#define BOARD_NAME  "FX01"
+#define BOARD_NAME  "Victor"
+
+//light segment sizes in Eric's room
+#define SEG_UP_START    0
+#define SEG_UP_END      23
+#define SEG_RIGHT_START 24
+#define SEG_RIGHT_END   61
+#define SEG_FRONT_START 62
+#define SEG_FRONT_END   93
+#define SEG_LEFT_START  94
+#define SEG_LEFT_END    132
+#define SEG_BACK_START  133
 
 #endif
 
