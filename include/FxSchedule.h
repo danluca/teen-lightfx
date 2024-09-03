7ebda773628c190bdca81ca911955ceadad52421
Dan Luca <danluca22@gmail.com>
2024-09-02 22:10:31 -0500

    updated for school year 24-25

--- include/FxSchedule.h
+++ include/FxSchedule.h
@@ -53,22 +53,32 @@ struct AlarmData {
 
 //weekends are implicit, no need to specify - see https://www.wayzataschools.org/calendar
 const Interval schoolSchedule[] PROGMEM = {
-        {.start=0x0101, .end=0x0103, .type=DayOff},     //new year
-        {.start=0x0103, .end=0x010F, .type=School},
-        {.start=0x010F, .end=0x0110, .type=DayOff},     //Martin Luther King
-        {.start=0x0110, .end=0x011A, .type=School},
-        {.start=0x011A, .end=0x011B, .type=DayOff},
-        {.start=0x011B, .end=0x020F, .type=School},
-        {.start=0x020F, .end=0x0214, .type=DayOff},     //President Day weekend
-        {.start=0x0214, .end=0x0308, .type=School},
-        {.start=0x0308, .end=0x0309, .type=DayOff},     //Flex learning day
-        {.start=0x0309, .end=0x031D, .type=School},
-        {.start=0x031D, .end=0x031E, .type=DayOff},
-        {.start=0x031E, .end=0x0409, .type=Vacation},   //spring break + one extra day 04/08
-        {.start=0x0409, .end=0x051B, .type=School},
-        {.start=0x051B, .end=0x051C, .type=DayOff},     //Memorial day
-        {.start=0x051C, .end=0x0607, .type=School},
-        {.start=0x0607, .end=0x0903, .type=Vacation},   //summer break
+        {.start=0x0101, .end=0x0102, .type=DayOff},     //new year
+        {.start=0x0102, .end=0x0114, .type=School},
+        {.start=0x0114, .end=0x0115, .type=DayOff},     //Martin Luther King
+        {.start=0x0115, .end=0x0118, .type=School},
+        {.start=0x0118, .end=0x0119, .type=DayOff},
+        {.start=0x0119, .end=0x020E, .type=School},
+        {.start=0x020E, .end=0x0212, .type=DayOff},     //President Day weekend
+        {.start=0x0212, .end=0x030E, .type=School},
+        {.start=0x030E, .end=0x030F, .type=DayOff},     //Flex learning day
+        {.start=0x030F, .end=0x031C, .type=School},
+        {.start=0x031C, .end=0x031D, .type=DayOff},
+        {.start=0x031D, .end=0x0407, .type=Vacation},   //spring break + one extra day 04/08
+        {.start=0x0407, .end=0x051A, .type=School},
+        {.start=0x051A, .end=0x051B, .type=DayOff},     //Memorial day
+        {.start=0x051B, .end=0x0606, .type=School},
+        {.start=0x0606, .end=0x0903, .type=Vacation},   //summer break
+        {.start=0x0903, .end=0x0917, .type=School},
+        {.start=0x0917, .end=0x0918, .type=DayOff},     //Flex learning day
+        {.start=0x0918, .end=0x0A10, .type=School},
+        {.start=0x0A10, .end=0x0A13, .type=DayOff},     //October educators day off
+        {.start=0x0A13, .end=0x0B08, .type=School},
+        {.start=0x0B08, .end=0x0B09, .type=DayOff},     //Flex learning day
+        {.start=0x0B09, .end=0x0B1B, .type=School},
+        {.start=0x0B1B, .end=0x0B1E, .type=DayOff},     //Thanksgiving weekend
+        {.start=0x0B1E, .end=0x0C17, .type=School},
+        {.start=0x0C17, .end=0x0C20, .type=Vacation},   //Christmas break
 };
 
 extern std::deque<AlarmData*> scheduledAlarms;
